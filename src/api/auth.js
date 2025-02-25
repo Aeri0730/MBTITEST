60ed1e2f9cfa58fade23619320b1189793337f0c
Aeri0730 <dodl8549@google.com>
2025-02-25 11:12:52 +0900

    [refactor]

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://www.nbcamp-react-auth.link";
+const API_URL = import.meta.env.VITE_AUTH_API_URL;
 export const register = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
